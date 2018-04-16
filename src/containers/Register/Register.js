d713e33f3bb8a7e9173ef4030771ebf00bc1fa87
Max Barry <max@mxbry.com>
2018-04-16 23:40:58 +0100

    Cleaned up some code in the register modal component

--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -2,51 +2,40 @@ import React, { Component } from 'react';
 
 import Modal from 'react-modal';
 import { withHandlers, compose } from 'recompose';
-import * as firebase from 'firebase/app';
+import firebase from 'firebase/app';
 import FirebaseAuth from 'react-firebaseui/FirebaseAuth';
 
 import urls from '../../settings/urls';
 
 Modal.setAppElement('#root');
 
-class Register extends Component {
-    render() {
-        const { closeModal, history } = this.props;
-        return (
-            <Modal
-                isOpen={true}
-                contentLabel="Register"
-                onRequestClose={closeModal}
-            >
-                <button onClick={closeModal}>Close</button>
-                <FirebaseAuth
-                    uiConfig={{
-                        // Popup signin flow rather than redirect flow.
-                        signInFlow: 'popup',
-                        // We will display Google and Facebook as auth providers.
-                        signInOptions: [
-                            {
-                                provider:
-                                    firebase.auth.GoogleAuthProvider
-                                        .PROVIDER_ID,
-                                authMethod: 'https://accounts.google.com',
-                                clientId:
-                                    '735729979405-b4dt8g88logn2me2q8lmau7sdjavjc8a.apps.googleusercontent.com'
-                            }
-                            // firebase.auth.FacebookAuthProvider.PROVIDER_ID
-                        ],
-                        credentialHelper: 'googleyolo',
-                        callbacks: {
-                            // Avoid redirects after sign-in.
-                            signInSuccess: () => history.push(urls.HOME)
-                        }
-                    }}
-                    firebaseAuth={firebase.auth()}
-                />
-            </Modal>
-        );
-    }
-}
+const Register = ({ closeModal, history }) => (
+    <Modal isOpen={true} contentLabel="Register" onRequestClose={closeModal}>
+        <button onClick={closeModal}>Close</button>
+        <FirebaseAuth
+            uiConfig={{
+                // Popup signin flow rather than redirect flow.
+                signInFlow: 'popup',
+                // We will display Google and Facebook as auth providers.
+                signInOptions: [
+                    {
+                        provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                        authMethod: 'https://accounts.google.com',
+                        clientId:
+                            '735729979405-b4dt8g88logn2me2q8lmau7sdjavjc8a.apps.googleusercontent.com'
+                    }
+                    // firebase.auth.FacebookAuthProvider.PROVIDER_ID
+                ],
+                credentialHelper: 'googleyolo',
+                callbacks: {
+                    // Avoid redirects after sign-in.
+                    signInSuccess: () => history.push(urls.HOME)
+                }
+            }}
+            firebaseAuth={firebase.auth()}
+        />
+    </Modal>
+);
 
 const enhance = compose(
     withHandlers({
