6e0480123f2c351533ac0b50249c41d633237317
Max Barry <max@mxbry.com>
2018-05-28 14:33:16 +0100

    Made rangecheckbox purely functional

--- src/components/Forms/RangeCheckbox.js
+++ src/components/Forms/RangeCheckbox.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { compose, setPropTypes } from 'recompose';
 import PropTypes from 'prop-types';
 import { cx } from 'emotion';
@@ -7,63 +7,32 @@ import {
     RangeListItem as ListItem,
     RangeCheck as Check,
     RangeLabel as Label,
-    // rangeNoPreviousClass as noPreviousClass,
-    // rangeNoNextClass as noNextClass,
     rangeActiveClass as activeClass
-    // RangeHandleWrap as HandleWrap,
-    // RangeHandle as Handle,
-    // RangeHandleConnector as Connector
-    // rangeActiveClass as activeClass
-    // rangeActiveCapClass as activeCapClass
 } from './RangeCheckbox.styles';
 import { makeAriaCheckboxProps, makeAriaLabelProps } from './Forms.shared';
 
-class RangeCheckbox extends Component {
-    render() {
-        const { items, onChange } = this.props;
-
-        return (
-            <List>
-                {items.map(({ label: Content, checked, id, disabled }, i) => {
-                    const classes = {
-                        // [noPreviousClass]:
-                        //     checked && (!items[i - 1] || !items[i - 1].checked),
-                        // [noNextClass]:
-                        //     checked && (!items[i + 1] || !items[i + 1].checked),
-                        [activeClass]: checked
-                    };
-                    return (
-                        <ListItem key={`listItem_${i}`}>
-                            <Label
-                                className={cx(classes)}
-                                {...makeAriaLabelProps(
-                                    checked,
-                                    id,
-                                    () => onChange(id),
-                                    disabled
-                                )}
-                            >
-                                <Check
-                                    {...makeAriaCheckboxProps(
-                                        checked,
-                                        id,
-                                        0,
-                                        disabled
-                                    )}
-                                />
-                                {typeof Content === 'function' ? (
-                                    <Content />
-                                ) : (
-                                    Content
-                                )}
-                            </Label>
-                        </ListItem>
-                    );
-                })}
-            </List>
-        );
-    }
-}
+const RangeCheckbox = ({ items, onChange, ...props }) => (
+    <List>
+        {items.map(({ label: Content, checked, id, disabled }, i) => (
+            <ListItem key={`listItem_${i}`}>
+                <Label
+                    className={cx({ [activeClass]: checked })}
+                    {...makeAriaLabelProps(
+                        checked,
+                        id,
+                        () => onChange(id),
+                        disabled
+                    )}
+                >
+                    <Check
+                        {...makeAriaCheckboxProps(checked, id, 0, disabled)}
+                    />
+                    {typeof Content === 'function' ? <Content /> : Content}
+                </Label>
+            </ListItem>
+        ))}
+    </List>
+);
 
 const enhance = compose(
     setPropTypes({
@@ -85,7 +54,6 @@ const enhance = compose(
             })
         ).isRequired
     })
-    // withAriaProps
 );
 
 export default enhance(RangeCheckbox);
