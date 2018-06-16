8a141e24bad2f59398f3c6d5b3fbb42a30ebabff
Max Barry <max@mxbry.com>
2018-06-16 11:36:42 +0100

    Removed dead code

--- src/containers/Filters/Facets/Facets.shared.js
+++ src/containers/Filters/Facets/Facets.shared.js
@@ -1,12 +1,4 @@
 import {
-    withHandlers,
-    compose,
-    lifecycle,
-    setPropTypes,
-    withStateHandlers
-} from 'recompose';
-import PropTypes from 'prop-types';
-import {
     prop,
     map,
     when,
@@ -19,68 +11,68 @@ import {
     filter
 } from 'ramda';
 
-export const withFacetLifecycle = lifecycle({
-    componentDidMount() {
-        if (this.props.onMount) this.props.onMount(this);
-    }
-});
+// export const withFacetLifecycle = lifecycle({
+//     componentDidMount() {
+//         if (this.props.onMount) this.props.onMount(this);
+//     }
+// });
 
-export const withListProcess = withHandlers({
-    process: ({ items }) => _ =>
-        items.filter(prop('isRefined')).map(prop('label'))
-});
+// export const withListProcess = withHandlers({
+//     process: ({ items }) => _ =>
+//         items.filter(prop('isRefined')).map(prop('label'))
+// });
 
-export const withListUpdate = withStateHandlers(
-    ({ initial }) => ({
-        lastUpdate: null,
-        items: initial
-    }),
-    {
-        update: ({ items }) => label => {
-            // Find the index of the value you wish to update
-            const i = items.findIndex(item => item.label === label);
-            const item = items[i];
-            // Update the element at index i with a new refined state
-            items[i] = { ...item, isRefined: !item.isRefined };
-            // Return the fresh new items
-            return {
-                items,
-                // We have this lastUpdate to trigger a state rerender.
-                // The items has changed deeply, so won't trigger rerender.
-                lastUpdate: new Date().getTime()
-            };
-        }
-    }
-);
+// export const withListUpdate = withStateHandlers(
+//     ({ initial }) => ({
+//         lastUpdate: null,
+//         items: initial
+//     }),
+//     {
+//         update: ({ items }) => label => {
+//             // Find the index of the value you wish to update
+//             const i = items.findIndex(item => item.label === label);
+//             const item = items[i];
+//             // Update the element at index i with a new refined state
+//             items[i] = { ...item, isRefined: !item.isRefined };
+//             // Return the fresh new items
+//             return {
+//                 items,
+//                 // We have this lastUpdate to trigger a state rerender.
+//                 // The items has changed deeply, so won't trigger rerender.
+//                 lastUpdate: new Date().getTime()
+//             };
+//         }
+//     }
+// );
 
-export const withUpdate = withStateHandlers(
-    ({ initial }) => ({
-        lastUpdate: null,
-        items: initial
-    }),
-    {
-        update: ({ items }) => label => {
-            // Find the index of the value you wish to update
-            const i = items.findIndex(item => item.label === label);
-            const item = items[i];
-            // Update the element at index i with a new refined state
-            items[i] = { ...item, isRefined: !item.isRefined };
-            // Return the fresh new items
-            return {
-                items,
-                // We have this lastUpdate to trigger a state rerender.
-                // The items has changed deeply, so won't trigger rerender.
-                lastUpdate: new Date().getTime()
-            };
-        }
-    }
-);
+// export const withUpdate = withStateHandlers(
+//     ({ initial }) => ({
+//         lastUpdate: null,
+//         items: initial
+//     }),
+//     {
+//         update: ({ items }) => label => {
+//             // Find the index of the value you wish to update
+//             const i = items.findIndex(item => item.label === label);
+//             const item = items[i];
+//             // Update the element at index i with a new refined state
+//             items[i] = { ...item, isRefined: !item.isRefined };
+//             // Return the fresh new items
+//             return {
+//                 items,
+//                 // We have this lastUpdate to trigger a state rerender.
+//                 // The items has changed deeply, so won't trigger rerender.
+//                 lastUpdate: new Date().getTime()
+//             };
+//         }
+//     }
+// );
 
-export const withFacetPropTypes = setPropTypes({
-    initial: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired
-});
+// export const withFacetPropTypes = setPropTypes({
+//     initial: PropTypes.oneOfType([PropTypes.array, PropTypes.object]).isRequired
+// });
 
-export const withShared = compose(withFacetPropTypes, withFacetLifecycle);
+// export const withShared = compose(withFacetPropTypes, withFacetLifecycle);
 
 // export const asFacetList = compose(withListUpdate, withListProcess, withShared);
 
