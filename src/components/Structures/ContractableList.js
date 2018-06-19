b173cc771810095502c14082b520d5c1b43f7947
Max Barry <max@mxbry.com>
2018-06-19 11:53:51 +0100

    ContractableList is in a healthy way.
    Now used a virtualized list for performance

--- src/components/Structures/ContractableList.js
+++ src/components/Structures/ContractableList.js
@@ -1,31 +1,124 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
+    List,
+    AutoSizer,
+    WindowScroller,
+    CellMeasurerCache,
+    CellMeasurer
+} from 'react-virtualized';
+import 'react-virtualized/styles.css';
+import {
     ContractableListContainer as Container,
-    ContractableListUl as Ul,
-    ContractableListLi as Li,
+    ContractableListRow as Row,
     ContractableListArea as Area
 } from './ContractableList.styles';
+import { dimensions } from '../../settings/styles';
 
-const ContractableList = ({ items, columns, children, ...props }) => (
-    <Container {...props}>
-        <Ul style={{ gridColumn: `span ${columns}` }}>
-            {items.map(({ component: Component, props }, i) => (
-                <Li key={`contract_${i}`}>
-                    <Component {...props} />
-                </Li>
-            ))}
-        </Ul>
-        <Area style={{ gridColumn: `span ${3 - columns}` }}>{children}</Area>
-    </Container>
-);
+// @link http://next.plnkr.co/edit/zjCwNeRZ7XtmFp1PDBsc?p=preview
+
+const TOTAL_COLUMNS = 3;
+
+const cache = new CellMeasurerCache({
+    defaultHeight: 360,
+    minHeight: 300,
+    fixedWidth: true
+});
+
+const ContractableList = ({
+    items,
+    columns,
+    children,
+    itemsCount,
+    sticky,
+    ...props
+}) => {
+    return (
+        <Container {...props}>
+            <WindowScroller>
+                {({
+                    height,
+                    width,
+                    isScrolling,
+                    registerChild,
+                    onChildScroll,
+                    scrollTop
+                }) => {
+                    const listSize = itemsCount || items.length;
+                    const adjustedWidth = width * (columns / TOTAL_COLUMNS);
+
+                    const itemsPerRow = Math.floor(
+                        adjustedWidth / dimensions.card
+                    );
+
+                    const rowCount = Math.ceil(listSize / itemsPerRow);
+
+                    const RowRenderer = ({ index, key, style, parent }) => {
+                        const rowItems = [];
+                        const fromIndex = index * itemsPerRow;
+                        const toIndex = Math.min(
+                            fromIndex + itemsPerRow,
+                            listSize
+                        );
+
+                        for (let i = fromIndex; i < toIndex; i++) {
+                            const { component: Component, props } = items[i];
+                            rowItems.push(<Component key={i} {...props} />);
+                        }
+
+                        return (
+                            <CellMeasurer
+                                cache={cache}
+                                columnIndex={0}
+                                key={key}
+                                parent={parent}
+                                rowIndex={index}
+                            >
+                                {() => <Row style={style}>{rowItems}</Row>}
+                            </CellMeasurer>
+                        );
+                    };
+
+                    return (
+                        <div
+                            ref={registerChild}
+                            style={{ gridColumn: `span ${columns}` }}
+                        >
+                            <List
+                                autoHeight
+                                height={height}
+                                isScrolling={isScrolling}
+                                onScroll={onChildScroll}
+                                rowCount={rowCount}
+                                rowRenderer={RowRenderer}
+                                width={adjustedWidth}
+                                deferredMeasurementCache={cache}
+                                rowHeight={cache.rowHeight}
+                                scrollTop={scrollTop}
+                                style={{ outline: 0 }}
+                            />
+                        </div>
+                    );
+                }}
+            </WindowScroller>
+            <Area
+                style={{ gridColumn: `span ${TOTAL_COLUMNS - columns}` }}
+                sticky={sticky}
+            >
+                {children}
+            </Area>
+        </Container>
+    );
+};
 
 ContractableList.defaultProps = {
-    columns: 3
+    columns: 3,
+    sticky: false
 };
 
 ContractableList.propTypes = {
     columns: PropTypes.number,
+    sticky: PropTypes.bool,
     items: PropTypes.arrayOf(
         PropTypes.shape({
             component: PropTypes.oneOfType([PropTypes.func, PropTypes.element])
--- src/components/Structures/ContractableList.styles.js
+++ src/components/Structures/ContractableList.styles.js
@@ -1,17 +1,21 @@
 import styled from 'react-emotion';
-import { bs, dimensions } from '../../settings/styles';
+import { bs } from '../../settings/styles';
 
 export const ContractableListContainer = styled('div')({
     display: 'grid',
-    gridTemplateColumns: 'repeat(3, 1fr)'
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    alignItems: 'start'
 });
 
-export const ContractableListUl = styled('ul')({
-    display: 'grid',
-    gridTemplateColumns: `repeat(auto-fill, minmax(${dimensions.card}px, 1fr))`,
-    gridGap: `${bs(2)} ${bs(1)}`
+export const ContractableListRow = styled('div')({
+    display: 'flex',
+    justifyContent: 'space-around',
+    padding: `${bs(0.5)} 0`
+    // display: 'grid',
+    // gridTemplateColumns: `repeat(auto-fill, minmax(${dimensions.card}px, 1fr))`,
+    // gridGap: `${bs(2)} ${bs(1)}`
 });
 
-export const ContractableListLi = styled('li')({});
-
-export const ContractableListArea = styled('div')({});
+export const ContractableListArea = styled('div')(
+    ({ sticky }) => (sticky ? { position: 'sticky', top: 0 } : {})
+);
